54de40fa7c953645674c696d7f63b3907be256e3
VictoriaShalabaeva <shalabaevavt@gmail.com>
2021-06-07 11:58:54 +0000

    Changed map style to dark

--- assets/js/send_email.js
+++ assets/js/send_email.js
@@ -1,3 +1,7 @@
+//The following code sends emails from the form
+
+//The code is written following the Code Institute tutorials and EmailJS documentation
+
 function sendMail(contactForm) {
     emailjs.send("service_jr9gthn","template_id_contact_form",{
         "from_name": contactForm.name.value,
@@ -6,12 +10,14 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
-            console.log("SUCCESS", response);
+            document.getElementById('contact-form').innerHTML = `<p>Your message has been sent successfully, we will be in touch with you within two working days.</p>`;
         },
         function(error) {
-            console.log("FAILED", error);
+            document.getElementById('contact-form').innerHTML = `<p>Your message was not sent, please try again.</p>`;
         }
     );
+    
+    return false;  // To block from loading a new page
+
     console.log("hello");
-    // return false;  // To block from loading a new page
 }
