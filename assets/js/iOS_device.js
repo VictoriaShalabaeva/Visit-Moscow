9c9614d6f19166998fe3a9fd48c7c17718873b25
VictoriaShalabaeva <shalabaevavt@gmail.com>
2021-07-06 08:29:23 +0000

    modified iOS_device.js

--- assets/js/iOS_device.js
+++ assets/js/iOS_device.js
@@ -1,26 +1,19 @@
 // Code credit: https://forums.mobirise.com/discussion/26407/iphone-and-background-size-cover
 
 
+(function($) {
+
 function iOSDetect() {
 
     $browser = strtolower($_SERVER['HTTP_USER_AGENT']); // Checks the user agent
 
     if(strstr($browser, 'iphone') || strstr($browser, 'ipad')) {
 
-        $device = 'iPhone';
-
-    } else { 
-        
-        $device = 'default'; 
-    }	
-
-    return($device);
+        $('#moscow-view').removeClass('image-outer-1').addClass('image-outer-1-ios');
+    }
 }
 
 iOSDetect();
 
-if(iOSDetect() == 'iPhone') {
-
-    $('#moscow-view').removeClass('image-outer-1').addClass('image-outer-1-ios');
-} 
+})(jQuery);
    
